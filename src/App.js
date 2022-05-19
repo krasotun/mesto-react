24f87ae254e078d255472dc0186ecf41f8c6d53c
krasotun <servrostov@gmail.com>
2022-05-19 23:48:00 +0300

    add Начал переностьь верстку

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import logoPath from "./images/logo.svg"
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="page__content">
+
+			<header className="header">
+				<img className="header__logo" src={logoPath} alt="Логотип" />
+			</header>
+
+			<main>
+				<section className="profile">
+					<div className="profile__content-container">
+						<div className="profile__avatar-container">
+							<img src="https://samcult.ru/wp-content/uploads/2021/01/johnny-rotten-1976-e1612102438221.jpg" alt="Аватар"
+								className="profile__avatar" />
+							<button className="profile__edit-avatar-button"></button>
+						</div>
+						<div className="profile__text-container">
+							<h1 className="profile__title">Марат Ибрагимов</h1>
+							<button className="profile__edit-button button"></button>
+							<h2 className="profile__subtitle">Веб-разработчик</h2>
+						</div>
+					</div>
+					<button className="profile__post-button button"></button>
+				</section>
+				<section className="elements">
+				</section>
+			</main>
+
+			<footer className="footer">
+				<p className="footer__text">© 2022 Mesto Russia</p>
+			</footer>
+
+		</div>
+	);
 }
 
 export default App;
