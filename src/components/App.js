9a230ef73139f18d69f8c705a042cc7198205b01
Marat <servrostov@gmail.com>
2022-05-21 13:44:39 +0300

    add Перенес попапы

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,14 @@
-
-// import './App.css';
-
 import Footer from "./Footer";
 import Header from "./Header";
+import Main from "./Main";
 
 function App() {
 	return (
 		<div className="page__content">
 			<Header />
-
-			<main>
-				<section className="profile">
-					<div className="profile__content-container">
-						<div className="profile__avatar-container">
-							<img src="https://samcult.ru/wp-content/uploads/2021/01/johnny-rotten-1976-e1612102438221.jpg" alt="Аватар"
-								className="profile__avatar" />
-							<button className="profile__edit-avatar-button"></button>
-						</div>
-						<div className="profile__text-container">
-							<h1 className="profile__title">Марат Ибрагимов</h1>
-							<button className="profile__edit-button button"></button>
-							<h2 className="profile__subtitle">Веб-разработчик</h2>
-						</div>
-					</div>
-					<button className="profile__post-button button"></button>
-				</section>
-				<section className="elements">
-				</section>
-			</main>
+			<Main />
 			<Footer />
+
 
 
 		</div>
