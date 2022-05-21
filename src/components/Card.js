453aee7cf25cb77ab5fc11450d8c5ca02ffd964b
krasotun <servrostov@gmail.com>
2022-05-21 22:53:35 +0300

    add Добавил заполнение карточками

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+function Card(props) {
+	return (
+		<article className="elements__card-container">
+			<div className="elements__delete">
+			</div>
+			<div className="elements__image-container">
+				<img className="elements__image" alt={props.name} src={props.link} />
+			</div>
+			<div className="elements__text-container">
+				<p className="elements__text">{props.name}</p>
+				<div className="elements__like-container">
+					<div className="elements__like"></div>
+					<span className="elements__like-count">{props.likes.length}</span>
+				</div>
+			</div>
+		</article>
+	)
+}
+
+export default Card
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { api } from "../utils/api";
+import Card from "./Card";
 
 
 function Main(props) {
-
 	const [userName, setUserName] = React.useState('Марат Ибрагимов');
 	const [userDescription, setUserDescription] = React.useState('Веб-разработчик');
 	const [userAvatar, setUserAvatar] = React.useState('https://www.m24.ru/b/d/nBkSUhL2jVghkceyPqzZvc62gYT28pj22yHFh_fH_nKUPXuaDyXTjHou4MVO6BCVoZKf9GqVe5Q_CPawk214LyWK9G1N5ho=6JaKLum5hY9EHmEeKsZ8Gw.jpg');
+	const [cards, setCards] = React.useState([]);
 
 	React.useEffect(() => {
 		api.getUserInfo()
@@ -19,8 +20,14 @@ function Main(props) {
 			})
 	}, [])
 
-
-
+	React.useEffect(() => {
+		api.getInitialCards()
+			.then((data) => {
+				setCards(data)
+			}).catch((r) => {
+				console.log(r);
+			})
+	}, [])
 	return (
 		<main>
 			<section className="profile">
@@ -38,7 +45,14 @@ function Main(props) {
 				</div>
 				<button className="profile__post-button button" onClick={props.onAddPlace}></button>
 			</section>
-			<section className="elements">
+			<section className="elements" >
+				{cards.map((card) =>
+					<Card
+						name={card.name}
+						link={card.link}
+						likes={card.likes}
+					/>
+				)}
 			</section>
 		</main>
 
