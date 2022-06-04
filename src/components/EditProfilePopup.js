3c717d9dd38cca66701eb45e9022ff00f9019ec2
Marat <servrostov@gmail.com>
2022-06-04 15:08:42 +0300

    add Настроил заполнение информацией о пользователе

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,6 +1,23 @@
+import React from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import PopupWithForm from "./PopupWithForm";
 
 function EditProfilePopup({ isOpen, onClose }) {
+	const [name, setName] = React.useState('')
+	const [description, setDescription] = React.useState('');
+
+	function handleNameChange(event) {
+		setName(event.target.value);
+	}
+	function handleDescriptionChange(event) {
+		setDescription(event.target.value)
+	}
+	const currentUser = React.useContext(CurrentUserContext);
+
+	React.useEffect(() => {
+		setName(currentUser.name);
+		setDescription(currentUser.about);
+	}, [currentUser]);
 
 	return (
 		<PopupWithForm
@@ -12,10 +29,10 @@ function EditProfilePopup({ isOpen, onClose }) {
 				<>
 					<fieldset className="form-edit__input-container">
 						<input minLength="2" maxLength="40" required className="form-edit__item form__item" id="name" type="text"
-							name="name" placeholder="Введите имя" />
+							name="name" placeholder="Введите имя" value={name} onChange={handleNameChange} />
 						<span className="form__error" id="name-error"></span>
 						<input minLength="2" maxLength="200" required className="form-edit__item form__item" id="about" type="text"
-							name="about" placeholder="Укажите профессию" />
+							name="about" placeholder="Укажите профессию" value={description} onChange={handleDescriptionChange} />
 						<span className="form__error" id="about-error"></span>
 					</fieldset>
 					<button type="submit" className="form-edit__submit-button form__submit-button button">
